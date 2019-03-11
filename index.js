6eb5440d2eee1f8e1aa9346ee845fe4b720e4a3c
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-03-11 19:40:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function logDriverNames(arrayOfDrivers){
+  arrayOfDrivers.forEach(function(driver){
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(arrayOfDrivers, location){
+  arrayOfDrivers.filter(
+    driver => driver.hometown === location).forEach(
+      driver => console.log(driver.name));
+}
+
+const comparer = function(driverOne, driverTwo){
+  return driverOne.revenue - driverTwo.revenue;
+}
+
+function driversByRevenue(arrayOfDrivers){
+  return arrayOfDrivers.slice().sort(comparer);
+}
+
+function stringSorter(driverOne, driverTwo){
+  return driverOne.name.localeCompare(driverTwo.name);
+}
+
+function driversByName(arrayOfDrivers){
+  return arrayOfDrivers.slice().sort(stringSorter);
+}
+
+function totalRevenue(arrayOfDrivers){
+  let sumArray = []
+  arrayOfDrivers.forEach(function(driver){
+    sumArray.push(driver.revenue);
+  });
+  return sumArray.reduce(function(a, b){ return a + b});
+}
+
+function averageRevenue(arrayOfDrivers){
+  return totalRevenue(arrayOfDrivers) / arrayOfDrivers.length;
+}
